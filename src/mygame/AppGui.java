dd580115ade904178336df2968e38b762d43cccc
davidTrimoulet <dtrimoulet@cesi.fr>
2014-03-20 22:49:17 +0100

    HudCorrect

--- src/mygame/AppGui.java
+++ src/mygame/AppGui.java
@@ -24,22 +24,22 @@ public class AppGui {
         this.startButton.setImage(app.getAssetManager(), "/Boutons/Buttonstart.png", true);
         this.startButton.setWidth(app.getMySettings().getWidth()/10);
         this.startButton.setHeight(app.getMySettings().getHeight()/10);
-        this.startButton.setPosition(0, app.getMySettings().getHeight()/2 - app.getMySettings().getHeight()/10);
+        this.startButton.setPosition(0, app.getMySettings().getHeight() - app.getMySettings().getHeight()/10);
         
         this.PauseButton = new Picture("PauseButton");
         this.PauseButton.setImage(app.getAssetManager(), "/Boutons/Buttonpause.png", true);
         this.PauseButton.setWidth(app.getMySettings().getWidth()/10);
         this.PauseButton.setHeight(app.getMySettings().getHeight()/10);
-        this.PauseButton.setLocalTranslation(0, app.getMySettings().getHeight()+ app.getMySettings().getHeight()/10, -1);
+        this.PauseButton.setPosition(0, app.getMySettings().getHeight() - ((app.getMySettings().getHeight()/10)*2));
         
         this.resumeButton = new Picture("resumeButton");
         this.resumeButton.setImage(app.getAssetManager(), "/Boutons/ButtonResume.png", true);
         this.resumeButton.setWidth(app.getMySettings().getWidth()/10);
         this.resumeButton.setHeight(app.getMySettings().getHeight()/10);
-        this.resumeButton.setPosition(0, app.getMySettings().getHeight() + app.getMySettings().getHeight()/10);
+        this.resumeButton.setPosition(0, app.getMySettings().getHeight() - ((app.getMySettings().getHeight()/10)*2));
         
         this.towerButton = new Picture("towerButton");
-        this.towerButton.setImage(app.getAssetManager(), "/Boutons/Buttonstart.png", true);
+        this.towerButton.setImage(app.getAssetManager(), "/Boutons/ButtonResume.png", true);
         this.towerButton.setWidth(app.getMySettings().getWidth()/10);
         this.towerButton.setHeight(app.getMySettings().getHeight()/10);
         this.towerButton.setPosition(app.getMySettings().getWidth() - app.getMySettings().getWidth()/10 , 0);
